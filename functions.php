538320bd5217ea45a72ec2e142010b97df971013
Maria Pimentel <maria.is.pimentel@gmail.com>
2024-01-14 18:11:58 +0000

    corrections to functions somehow worked, new edits coming up

--- functions.php
+++ functions.php
@@ -1,4 +1,12 @@
 <?php
 
+function mp_enqueue_styles(){
+    wp_enqueue_style('mp-style', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
+    wp_enqueue_style('mp-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" . "/style.css", array(), '1.0', 'all');
+    wp_enqueue_style('mp-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" . "/style.css", array(), '1.0', 'all');
+}
+
+add_action('wp_enqueue_scripts', 'mp_enqueue_styles');
 
 ?>
+
