aa61f43d307a4ea0897cbbb05bc2bbfbca23700b
Maria Pimentel <maria.is.pimentel@gmail.com>
2024-01-14 18:30:20 +0000

    version parameter for style theme

--- functions.php
+++ functions.php
@@ -1,7 +1,9 @@
 <?php
 
 function mp_enqueue_styles(){
-    wp_enqueue_style('mp-style', get_template_directory_uri() . '/style.css', array('mp-bootstrap'), '1.0', 'all');
+
+    $version = wp_get_theme()->get( 'Version');
+    wp_enqueue_style('mp-style', get_template_directory_uri() . '/style.css', array('mp-bootstrap'), $version, 'all');
     wp_enqueue_style('mp-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all');
     wp_enqueue_style('mp-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all');
 }
