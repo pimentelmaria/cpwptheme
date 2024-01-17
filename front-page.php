073e7b3e256ddd413c77d1ec15983da9d0b47b05
Maria Pimentel <maria.is.pimentel@gmail.com>
2024-01-17 13:01:15 +0000

    copyright

--- front-page.php
+++ front-page.php
@@ -120,7 +120,7 @@
 	    </article>
 	    <footer class="footer text-center py-2 theme-bg-dark">
 		   
-            <p class="copyright"><a href="https://youtube.com/FollowAndrew">FollowAndrew</a></p>
+            <p class="copyright">Copyright</p>
 		   
 	    </footer>
     
